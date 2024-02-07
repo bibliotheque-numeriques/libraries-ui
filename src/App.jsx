8f16ca3b59570eb853f7a28173f2933f079c6d2b
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-02-07 14:50:51 +0300

    feat : add react router dom and axios  dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Admin from "./page/admin/Admin";
 import Login from "./page/login/Login";
+import Landing from "./page/singlepage/Landing";
 function App() {
   return (
-    <>
-      {/* <Login /> */}
-      <Admin />
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Landing />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
