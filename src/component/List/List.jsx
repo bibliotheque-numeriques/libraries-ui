813ab3d383088b67eaf20ab8a9b1ebec5f7775b9
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-02-07 22:48:05 +0300

    feat: add script to get book to database

--- src/component/List/List.jsx
+++ src/component/List/List.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./List.css";
-function List(title, page, Langage, Parution) {
+function List({ title, page, author, category, Parution, langage }) {
   return (
     <div className="list-item">
       <p>{title}</p>
       <p>{page}</p>
-      <p>{Langage}</p>
+      <p>{author}</p>
+      <p>{category} </p>
       <p>{Parution}</p>
+      <p>{langage} </p>
     </div>
   );
 }
