d3a644b5ab376bcc344fd9d9ff176111fd392ef0
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-02-13 10:12:38 +0300

    feat: add Filter and Addbook component

--- src/component/Content/Content.jsx
+++ src/component/Content/Content.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ContentHeader from "../ContentHeader/ContentHeader";
 import ListContent from "../ListContent/ListContent";
+import AddBook from "../AddBook/AddBook";
+import Filter from "../Filterbar/Filter";
 
 function Content() {
   return (
     <div className="content">
       <ContentHeader />
+      <AddBook />
+      <Filter />
       <ListContent />
     </div>
   );
