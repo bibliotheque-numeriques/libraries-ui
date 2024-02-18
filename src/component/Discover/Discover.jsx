c7424418c08540a28cdd73b912f701f7052175ba
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-02-18 19:03:32 +0300

    feat: create discover component

--- src/component/Discover/Discover.jsx
+++ src/component/Discover/Discover.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import "./Discover.css";
+import imagebanner from "../../ressource/imagePannel.svg";
 function Discover() {
-  return <div>Discover</div>;
+  return (
+    <div className="discover-content">
+      <div className="banner">
+        <div className="banner-text">
+          <h4>Welcome to e-library dashboard</h4>
+          <p>
+            Discover a world of knowledge at your fingertips. Explore our
+            digital library
+          </p>
+        </div>
+        <div className="banner-img">
+          <img src={imagebanner} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Discover;
